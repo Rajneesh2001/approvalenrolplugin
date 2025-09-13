3b68b40e4ee7890bef9f20511391d2ccb95ca3ee
sajwanrajneesh81@gmail.com <sajwanrajneesh81@gmail.com>
2025-09-13 14:25:22 +0000

    Add install.php to create new role named Approver

--- db/install.php
+++ db/install.php
@@ -1,12 +1,18 @@
 <?php
 
-function xmldb_approvalenrol_install(){
-    $roleid = create_role('Approver', 'approver', 'This Allow user to approve the request of users who want to enrol specific course');
-    assign_capability(
-        'enrol/approvalenrol:manage',
-        CAP_ALLOW,
-        $roleid,
-        (\context_system::instance())->id,
-        true
-    );
+function xmldb_enrol_approvalenrol_install()
+{
+    global $DB;
+    if (!$DB->record_exists('role', ['shortname' => 'approver'])) {
+        $roleid = create_role(get_string('approverole', 'enrol_approvalenrol'), 'approver', get_string('approverrole:desc', 'enrol_approvalenrol'));
+        if($roleid){
+            assign_capability(
+            'enrol/approvalenrol:manage',
+            CAP_ALLOW,
+            $roleid,
+            (\context_system::instance())->id,
+            true
+        );
+        }
+    }
 }

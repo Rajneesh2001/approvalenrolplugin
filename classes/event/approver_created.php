cb3ebf6066c0df569ea1211b7572b5f537c74aac
sajwanrajneesh81@gmail.com <sajwanrajneesh81@gmail.com>
2025-09-25 18:27:07 +0000

    Add event triggered when approver is created

---
+++ classes/event/approver_created.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace enrol_approvalenrol\event;
+
+use core\event\base;
+
+/**
+ * Event triggered when a new approver record is created in the enrol_approvalenrol_approvers table.
+ */
+class approver_created extends base
+{
+
+    /**
+     * Initialize the event
+     * @return void
+     */
+    public function init()
+    {
+        $this->data['crud'] = 'c';
+        $this->data['edulevel'] = self::LEVEL_OTHER;
+        $this->data['objecttable'] = 'enrol_approvalenrol_approvers';
+    }
+
+    /**
+     * Returns localized event name
+     * @return string event name
+     */
+    public static function get_name()
+    {
+        return get_string('approvercreated', 'enrol_approvalenrol');
+    }
+
+    /**
+     * Returns localized description of what happened
+     * @return string event description
+     */
+    public function get_description()
+    {
+        $descriptionobj = new \stdClass();
+        $descriptionobj->userid = $this->userid;
+        $descriptionobj->courseid = $this->courseid;
+        return get_string('approvercreated:desc', 'enrol_approvalenrol', $descriptionobj);
+    }
+
+    /**
+     * Returns the URL relevant to the approver created event.
+     *
+     * @return \moodle_url
+     */
+    public function get_url()
+    {
+        return new \moodle_url('/enrol/approvalenrol/select_approver.php', ['courseid' => $this->context->instanceid]);
+    }
+
+    /**
+     * Return objectid mapping for backup/restore.
+     *
+     * @return array
+     */
+    public static function get_objectid_mapping()
+    {
+        return ['db' => 'enrol_approvalenrol_approvers', 'restore' => 'enrol_approvalenrol_approvers'];
+    }
+
+    /**
+     * Define Mapping for additional data fields
+     */
+    public static function get_other_mapping()
+    {
+        return [];
+    }
+}

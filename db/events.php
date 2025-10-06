47752e5991f9664f2272070b79b1fb1a8fdad9dd
sajwanrajneesh81@gmail.com <sajwanrajneesh81@gmail.com>
2025-10-06 18:18:49 +0000

    Attach observer to core config created event

--- db/events.php
+++ db/events.php
@@ -4,5 +4,9 @@ $observers = [
     [
         'eventname' => '\enrol_approvalenrol\event\approval_requests_updated',
         'callback' => '\enrol_approvalenrol\event\observer::process_request',
+    ],
+    [
+        'eventname' => '\core\event\config_log_created',
+        'callback' => '\enrol_approvalenrol\event\observer::save_config_data'
     ]
 ];

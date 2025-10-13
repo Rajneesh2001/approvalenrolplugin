72f512497aa1b073790bb7647bdb49a55511a05a
sajwanrajneesh81@gmail.com <sajwanrajneesh81@gmail.com>
2025-10-13 14:11:03 +0000

    Modify access.php to remove default capability for archetypes

--- db/access.php
+++ db/access.php
@@ -3,16 +3,10 @@
 $capabilities= [
     'enrol/approvalenrol:config' => [
         'captype' => 'write',
-        'contextlevel' => CONTEXT_COURSE,
-        'archetypes'=>[
-            'manager' => CAP_ALLOW
-        ]
+        'contextlevel' => CONTEXT_COURSE
     ],
     'enrol/approvalenrol:manage' => [
         'captype' => 'write',
-        'contextlevel' => CONTEXT_COURSE,
-        'archetypes' => [
-            'manager' => CAP_ALLOW
-        ]
+        'contextlevel' => CONTEXT_COURSE
     ],
 ];

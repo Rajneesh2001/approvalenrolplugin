3c09d882df14d4f04664b62496796fad0a70c0ae
sajwanrajneesh81@gmail.com <sajwanrajneesh81@gmail.com>
2026-02-25 05:47:11 +0000

    fix: restrict message providers with capability check

--- db/messages.php
+++ db/messages.php
@@ -4,7 +4,8 @@ $messageproviders = [
     'approval_notifications' => [
         'defaults' => [
             'pop-up' => MESSAGE_PERMITTED,
-            'email' => MESSAGE_PERMITTED
-        ]
+            'email' => MESSAGE_PERMITTED,
+        ],
+        'capability' => 'enrol/approvalenrol:config',
     ]
 ];

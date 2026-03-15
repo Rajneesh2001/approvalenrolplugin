ad6ed747a3c7adbbdbe7e1b251527a9ac18abd91
sajwanrajneesh81@gmail.com <sajwanrajneesh81@gmail.com>
2026-03-15 17:58:13 +0000

    Add plugin release and Maturity to version.php

---
+++ version.php
@@ -0,0 +1,7 @@
+<?php
+
+$plugin->version = 2025100100;
+$plugin->requires = 2023110100;
+$plugin->component = 'enrol_approvalenrol';
+$plugin->release = "0.1.0";
+$plugin->maturity = 'MATURITY_ALPHA';
